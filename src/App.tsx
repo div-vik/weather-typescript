04365da2cef888f36fb7e6461630e7c7d1343cd0
div-vik <shivamambast5@gmail.com>
2023-10-11 21:33:33 +0530

    search input works

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,69 @@
-const App = () => {
+import { ChangeEvent, useState } from "react";
+import { optionType } from "./types";
+
+const App = (): JSX.Element => {
+  const [term, setTerm] = useState<string>("");
+  const [options, setOptions] = useState<[]>([]);
+
+  const getSearchOptions = (value: string) => {
+    fetch(
+      `https://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=6&appid=0925bb00388a48a9bd7d779ec225299d`
+    )
+      .then((res) => res.json())
+      .then((data) => setOptions(data));
+  };
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+    setTerm(value);
+
+    if (value === "") return;
+
+    getSearchOptions(value);
+  };
+
+  const onOptionSelect = (option: optionType) => {
+    fetch(
+      `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`
+    );
+  };
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      Hello world!!
+      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls drop-shadow-lg rounded text-zinc-700">
+        <h1 className="text-4xl font-thin">
+          Weather <span className="font-black">Forecast</span>
+        </h1>
+
+        <p className="text-sm mt-2">
+          Enter below a place you want to know the weather of and select an
+          option from the dropdown
+        </p>
+
+        <div className="relative flex mt-10 md:mt-4">
+          <input
+            type="text"
+            value={term}
+            onChange={onInputChange}
+            className="px-2 py-1 rounded-l-md border-2 border-white"
+          />
+          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
+            {options.map((option: optionType, index: number) => (
+              <li key={option.name + "-" + index}>
+                <button
+                  onClick={() => onOptionSelect(option)}
+                  className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer"
+                >
+                  {option.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+
+          <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer">
+            Search
+          </button>
+        </div>
+      </section>
     </main>
   );
 };
